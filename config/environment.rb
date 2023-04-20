e958b8071323996aae7435634ca36a7f0e0ec809
Daiya Usui <1016daiya@gmail,com>
2023-04-20 20:55:27 +0900

    a

---
+++ config/environment.rb
@@ -0,0 +1,13 @@
+# config/environment.rb
+
+# Load the Bundler setup
+require 'bundler/setup'
+
+# Load gems
+require 'discordrb'
+require 'date'
+require 'json'
+require 'open-uri'
+
+# Application specific configuration (if needed)
+# ...
